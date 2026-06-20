3a403dbde80a1e9cdffb98cf1bb5ff4e093b9438
pjydud <44135584+pjydud@users.noreply.github.com>
2026-06-21 04:55:55 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "spanish-30-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./data.js",
+  "./manifest.json",
+  "./test.html",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => caches.match("./index.html"));
+    })
+  );
+});
